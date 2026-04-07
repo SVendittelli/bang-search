49427e0f14c9dd9958c434211c4a5dd91f9a660b
Sam Vendittelli <sam.vendittelli@hotmail.com>
2026-04-08 00:46:35 +0100

    feat: add Starling Bank GitHub search bangs
    - Add !ghsb for searching code in starlingbank org
    - Add !ghsbr for searching repositories in starlingbank org

--- custom-bangs.js
+++ custom-bangs.js
@@ -4,6 +4,24 @@
 export const customBangs = [
   {
     c: "Tech",
+    d: "github.com",
+    r: 148,
+    s: "Starling Bank GitHub Code Search",
+    sc: "Programming",
+    t: "ghsb",
+    u: "https://github.com/search?q=org%3Astarlingbank+{{{s}}}&type=code",
+  },
+  {
+    c: "Tech",
+    d: "github.com",
+    r: 149,
+    s: "Starling Bank GitHub Repo Search",
+    sc: "Programming",
+    t: "ghsbr",
+    u: "https://github.com/search?q=org%3Astarlingbank+{{{s}}}&type=repositories",
+  },
+  {
+    c: "Tech",
     d: "npmx",
     r: 147,
     s: "npmx",
