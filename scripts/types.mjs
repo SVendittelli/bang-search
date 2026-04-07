566c24b93dad423d09969a7083bba33ef0fd7733
Sam Vendittelli <sam.vendittelli@hotmail.com>
2026-04-07 23:00:40 +0100

    chore: start over

--- scripts/types.mjs
+++
@@ -1,8 +0,0 @@
-/**
- * A bang is a shortcut to search a specific site.
- *
- * @typedef {Object} Bang
- * @property {string} s - Name
- * @property {string} t - Bang
- * @property {string} u - URL
- */
